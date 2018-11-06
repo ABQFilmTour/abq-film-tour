11e1792fa1a01e009dc18d4d817b45b61ead36c3
rebfarish <rebfarish@gmail.com>
2018-11-06 11:48:14 -0700

    Added UserComments Dao.

--- app/src/main/java/edu/cnm/deepdive/abq_film_tour/model/entity/UserComments.java
+++ app/src/main/java/edu/cnm/deepdive/abq_film_tour/model/entity/UserComments.java
@@ -5,13 +5,15 @@ import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.ForeignKey;
 import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+import java.util.Date;
 
 @Entity(
     foreignKeys =  {
-        @ForeignKey(entity = FilmLocation.class, parentColumns = "film_location_id",
-            childColumns = "film_location_id", onDelete = ForeignKey.CASCADE),
-        @ForeignKey(entity = User.class, parentColumns = "user_id",
-            childColumns = "user_id", onDelete = ForeignKey.CASCADE)
+        @ForeignKey(entity = FilmLocation.class, parentColumns = "filmLocationId",
+            childColumns = "filmLocationId", onDelete = ForeignKey.CASCADE),
+        @ForeignKey(entity = User.class, parentColumns = "userId",
+            childColumns = "userId", onDelete = ForeignKey.CASCADE)
     }
 
 
@@ -24,12 +26,26 @@ public class UserComments {
   private long id;
 
   @ColumnInfo(name = "film_location_id")
-  private long film_location_id;
+  private long filmLocationId;
 
   @ColumnInfo(name = "user_id")
-  private long user_id;
+  private long userId;
 
+  @NonNull
+  @ColumnInfo(index = true)
+  private Date timestamp = new Date();
+
+  @ColumnInfo(name = "content")
   private String content;
+
+  @NonNull
+  public Date getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(@NonNull Date timestamp) {
+    this.timestamp = timestamp;
+  }
 
   public long getId() {
     return id;
@@ -39,20 +55,20 @@ public class UserComments {
     this.id = id;
   }
 
-  public long getFilm_location_id() {
-    return film_location_id;
+  public long getFilmLocationId() {
+    return filmLocationId;
   }
 
-  public void setFilm_location_id(long film_location_id) {
-    this.film_location_id = film_location_id;
+  public void setFilmLocationId(long filmLocationId) {
+    this.filmLocationId = filmLocationId;
   }
 
-  public long getUser_id() {
-    return user_id;
+  public long getUserId() {
+    return userId;
   }
 
-  public void setUser_id(long user_id) {
-    this.user_id = user_id;
+  public void setUserId(long userId) {
+    this.userId = userId;
   }
 
   public String getContent() {
