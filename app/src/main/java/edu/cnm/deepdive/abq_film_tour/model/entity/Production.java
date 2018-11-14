42056f3bec9bc7aa3071c12412e0d609cbc431d1
rebfarish <rebfarish@gmail.com>
2018-11-13 17:55:49 -0700

    Minor refactoring changes.

--- app/src/main/java/edu/cnm/deepdive/abq_film_tour/model/entity/Production.java
+++ app/src/main/java/edu/cnm/deepdive/abq_film_tour/model/entity/Production.java
@@ -3,13 +3,56 @@ package edu.cnm.deepdive.abq_film_tour.model.entity;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
 import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+import edu.cnm.deepdive.abq_film_tour.model.entity.FilmLocation;
+import java.util.Date;
 
-@Entity
+@Entity(
+    foreignKeys = {
+        @ForeignKey(entity = FilmLocation.class, parentColumns = "film_location_id",
+            childColumns = "film_location_id", onDelete = ForeignKey.CASCADE)
+    }
+)
+
+
 public class Production {
 
+
+  @NonNull
+  @ColumnInfo(index = true)
+  private Date timestamp = new Date();
+
+  @ColumnInfo(name="type")
+  private String type;
 
   @PrimaryKey(autoGenerate = true)
   @ColumnInfo(name="production_id")
   private long id;
+
+  @NonNull
+  public Date getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(@NonNull Date timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
 }
