812ccece0490bb36349c8fabb8c6b516e78c2a07
rebfarish <rebfarish@gmail.com>
2018-12-02 21:04:19 -0700

    Changed styling of dialog fragment to hold more than one item and made transparent.

--- app/src/main/java/edu/cnm/deepdive/abq_film_tour/controller/SelectionDialog.java
+++ app/src/main/java/edu/cnm/deepdive/abq_film_tour/controller/SelectionDialog.java
@@ -2,6 +2,9 @@ package edu.cnm.deepdive.abq_film_tour.controller;
 
 
 import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,6 +13,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -24,21 +29,16 @@ public class SelectionDialog extends DialogFragment {
   private static final String TITLE_LIST_KEY="titlesList";
   private static final String TITLE_SELECTION_KEY="titleSelection";
 
-  private RecyclerView tvRecyclerView;
-  private TextView titleTextView;
-  private List<String> locations;
-
-  public SelectionDialog() {
-    // Required empty public constructor
-  }
-
-
 
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_selection, null);
+    //transparent color for dialog fragment
+    final Drawable d = new ColorDrawable(Color.BLACK);
+    d.setAlpha(130);
+    View view = inflater.inflate(R.layout.fragment_selection, container,false);
+
     ListView selectionListView = view.findViewById(R.id.selection_list_view);
     Bundle arguments = this.getArguments();
     assert arguments != null;
@@ -46,7 +46,26 @@ public class SelectionDialog extends DialogFragment {
     ArrayAdapter<String> adapter = new ArrayAdapter<>(Objects.requireNonNull(getActivity()),
         android.R.layout.simple_list_item_1, titles);
     selectionListView.setAdapter(adapter);
+    //sets dialog fragment to transparent color
+    getDialog().getWindow().setBackgroundDrawable(d);
     return view;
   }
+
+
+//  @Override
+//  public void onResume() {
+//    ViewGroup.LayoutParams params = getDialog().getWindow().getAttributes();
+//    params.width = LayoutParams.MATCH_PARENT;
+//    params.height = LayoutParams.WRAP_CONTENT;
+//    //here I tried changing params
+//    getDialog().getWindow().setAttributes((android.view.WindowManager.LayoutParams)params);
+//
+//    another way also doesn't work:
+//
+//    int width = getResources().getDimensionPixelSize(R.dimen.popup_width);
+//    int height = getResources().getDimensionPixelSize(R.dimen.popup_height);
+//    getDialog().getWindow().setLayout(width, height);
+//    super.onResume();
+//  }
 }
 
