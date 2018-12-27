6aee4856c0ade64cbd132fb5005a3b7360e9f066
Joseph Peterson <jscpeterson@gmail.com>
2018-12-27 13:38:37 -0700

    Updated string message for null location

--- app/src/main/java/edu/cnm/deepdive/abq_film_tour/controller/MapsActivity.java
+++ app/src/main/java/edu/cnm/deepdive/abq_film_tour/controller/MapsActivity.java
@@ -549,6 +549,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // If request is cancelled, the result arrays are empty.
         if (grantResults.length > 0
             && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+          //TODO Optimize for more efficient battery usage
           locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
               2000, //Milliseconds
               10, //Distance
