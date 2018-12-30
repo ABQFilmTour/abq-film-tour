fee9a8b4d81c1e948b9dc9fcabba7c93e697bec4
samuelandazola <szola3@icloud.com>
2018-12-29 20:24:24 -0600

    fixed submit dialog, changed location activity layout

--- app/src/main/java/edu/cnm/deepdive/abq_film_tour/controller/SubmitCommentDialog.java
+++ app/src/main/java/edu/cnm/deepdive/abq_film_tour/controller/SubmitCommentDialog.java
@@ -3,27 +3,34 @@ package edu.cnm.deepdive.abq_film_tour.controller;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
 import edu.cnm.deepdive.abq_film_tour.R;
 
 public class SubmitCommentDialog extends DialogFragment implements View.OnClickListener {
 
-  Button userCommentButton;
 
-  private TextInputLayout userCommentInput;
   @Nullable
-  public View onCreate(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+
     View view = inflater.inflate(R.layout.submit_comment_fragment, null, false);
-    userCommentInput = view.findViewById(R.id.user_comment_input);
-    userCommentButton = view.findViewById(R.id.user_comment_button);
+
     return view;
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+
+    ViewGroup.LayoutParams params = getDialog().getWindow().getAttributes();
+    params.width = LayoutParams.MATCH_PARENT;
+    getDialog().getWindow().setAttributes((WindowManager.LayoutParams) params);
   }
 
   @Override
